7e3cc4432f8744fcd9c9d0280712396a15f9b055
Minh-Duc Bui <minhduc2k31905@gmail.com>
2025-01-17 18:49:52 +0700

    ✨ feat: add video processing for speed estimation

--- src/speed_estimation.py
+++ src/speed_estimation.py
@@ -1,34 +1,43 @@
 import cv2
 from ultralytics import solutions
 
-cap = cv2.VideoCapture("samples/fruit_and_vegetable.gif")
+from src.utils import logger
+
+video_path = "samples/highway-2.mp4"
+
+cap = cv2.VideoCapture(video_path)
 assert cap.isOpened(), "Error reading video file"
+
 w, h, fps = (
     int(cap.get(x))
     for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS)
 )
 
+logger.info(f"Processing video with {fps} fps, {w}x{h} resolution")
+
 # Video writer
-video_writer = cv2.VideoWriter(
-    "speed_management.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
-)
+file_name = f"./run/{video_path.split('/')[-1].split('.')[0]}_speed_estimated.mp4"
+video_writer = cv2.VideoWriter(file_name, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
 # Define speed region points
-speed_region = [(20, 400), (1080, 400), (1080, 360), (20, 360)]
+speed_region = [
+    (int(w * 0.2), int(h * 0.3)),  # Top left
+    (int(w * 0.9), int(h * 0.3)),  # Top right
+    (int(w * 0.9), h),  # Bottom right
+    (int(w * 0.2), h),  # Bottom left
+]
 
 speed = solutions.SpeedEstimator(
-    show=True,  # Display the output
-    model="yolo11n.pt",  # Path to the YOLO11 model file.
-    region=speed_region,  # Pass region points
-    # classes=[0, 2],  # If you want to estimate speed of specific classes.
-    # line_width=2,  # Adjust the line width for bounding boxes and text display
+    show=True,
+    model="yolo11n.pt",
+    region=speed_region,
 )
 
 # Process video
 while cap.isOpened():
     success, im0 = cap.read()
     if not success:
-        print(
+        logger.info(
             "Video frame is empty or video processing has been successfully completed."
         )
         break
@@ -38,3 +47,5 @@ while cap.isOpened():
 cap.release()
 video_writer.release()
 cv2.destroyAllWindows()
+
+logger.info(f"Speed estimation video saved to {file_name}")
