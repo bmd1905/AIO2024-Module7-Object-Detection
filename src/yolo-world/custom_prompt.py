28e6d400000a7f4d8aec6373a9966154e7c1e740
Minh-Duc Bui <minhduc2k31905@gmail.com>
2025-01-22 17:28:48 +0700

    ✨ feat: update YOLO model and add sample images
    - Changed YOLO model from 'yolov8s-world.pt' to 'yolov8x-world.pt' for improved performance.
    - Updated detection classes to include 'phone', 'mask', and 'glasses'.
    - Modified prediction input image to 'samples/vietnam-3.jpg'.
    - Added new sample images: 'nguyen_hue.jpg', 'sample.jpg', 'sample2.png', 'vietnam-3.jpg', and 'vietnam-4.jpg' to the samples directory.
    - Updated devcontainer configuration to ensure proper setup for Python development.

--- src/yolo-world/custom_prompt.py
+++ src/yolo-world/custom_prompt.py
@@ -4,13 +4,17 @@ from ultralytics.engine.results import Boxes
 from src.utils import save_detection_results
 
 # Initialize a YOLO-World model
-model = YOLOWorld("yolov8s-world.pt")
+model = YOLOWorld("yolov8x-world.pt")
 
 # Define custom classes
-model.set_classes(["bus"])  # <--------- Change this to the class you want to detect
+model.set_classes(
+    ["phone", "mask", "glasses"]
+)  # <--------- Change this to the class you want to detect
 
 # Execute prediction on an image
-results: Boxes = model.predict("samples/bus.jpg")
+results: Boxes = model.predict(
+    "samples/vietnam-3.jpg", max_det=100, iou=0.01, conf=0.01
+)
 
 # Save detection results as images
 save_detection_results(results)
